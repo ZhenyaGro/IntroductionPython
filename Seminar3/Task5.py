e1480392fa70cc9b4d2e42118a12d0a71f1c6646
ZhenyaGro <bon4ios@gmail.com>
2022-10-04 22:35:48 +0300

    Seminar 3 is completed

--- Seminar3/Task5.py
+++ Seminar3/Task5.py
@@ -11,6 +11,23 @@ def fibonacci(n):
         return fibonacci(n - 1) + fibonacci(n - 2)
 
 
+def nega_fibonacci(n):
+    if n == 0:
+        return 0
+    if n == -1:
+        return 1
+    if n == -2:
+        return -1
+    else:
+        return nega_fibonacci(n + 2) - nega_fibonacci(n + 1)
+
+
 number = 8
+result = []
+for i in range(-number, 0):
+    result.append(nega_fibonacci(i))
+
 for i in range(number + 1):
-    print(fibonacci(i))
+    result.append(fibonacci(i))
+
+print(result)
