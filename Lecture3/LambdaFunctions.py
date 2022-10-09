da276a1ab60bfefda58fcef2539ba8f3dce2b1d1
ZhenyaGro <bon4ios@gmail.com>
2022-10-09 22:26:05 +0300

    Example

--- Lecture3/LambdaFunctions.py
+++ Lecture3/LambdaFunctions.py
@@ -3,7 +3,12 @@
 # получить [(2, 4), (8, 64), (38, 1444)]
 
 file = open(f'Lecture3/numbers.txt', 'r')
-result = [(i, int(i) * int(i))
-          for i in file.read().split(' ') if int(i) % 2 == 0]
+# result = [(i, int(i) * int(i))
+#          for i in file.read().split() if int(i) % 2 == 0]
+
+res = map(int, file.read().split())
 file.close()
-print(result)
+res = filter(lambda x: not x % 2, res)
+res = list(map(lambda x: (x, x ** 2), res))
+
+print(res)
