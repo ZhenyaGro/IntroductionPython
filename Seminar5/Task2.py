3b9b6bafd805e806e5ed2c70dff2eee5357a6ac6
ZhenyaGro <bon4ios@gmail.com>
2022-10-10 20:27:48 +0300

    Task 2 in progress...

---
+++ Seminar5/Task2.py
@@ -0,0 +1,64 @@
+# Создайте программу для игры с конфетами человек против человека.¡
+# Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга. Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход. Сколько конфет нужно взять первому игроку, чтобы забрать все конфеты у своего конкурента?
+# a) Добавьте игру против бота
+# b) Подумайте как наделить бота ""интеллектом""
+
+from random import randint
+
+
+def choose_mode():
+    game_mode = input(
+        'Выбор режима игры.\n1 - Против игрока\n2 - Против бота\nРежим: ')
+    if game_mode != '1' and game_mode != '2':
+        print('Выбран некорректный режим')
+
+    return game_mode
+
+
+count_candy = 120
+
+
+def pvp(candy):
+    print(f'Количество конфет: {candy}')
+    current_player = randint(1, 2)
+    print(f'Первым ходит: Игрок {current_player}')
+    if current_player == 1:
+        first_player = True
+    else:
+        first_player = False
+
+    while candy != 0:
+        if first_player == True:
+            current_player = 1
+        elif first_player == False:
+            current_player = 2
+
+        print(
+            f'\nХод Игрока {current_player}\nТекущее количество конфет: {candy}')
+
+        if candy >= 28:
+            take_candy = int(input('Заберите от 1 до 28 конфет: '))
+            while take_candy < 1 or take_candy > 28:
+                print('Некорректное количество конфет. Попробуйте еще раз')
+                take_candy = int(input('Заберите от 1 до 28 конфет: '))
+        else:
+            take_candy = int(input(f'Заберите от 1 до {candy} конфет: '))
+            while take_candy < 1 or take_candy > candy:
+                print('Некорректное количество конфет. Попробуйте еще раз')
+                take_candy = int(input(f'Заберите от 1 до {candy} конфет: '))
+
+        candy -= take_candy
+        first_player = not first_player
+
+    return
+
+
+def pve(candy):
+    return
+
+
+game_mode = choose_mode()
+if game_mode == '1':
+    pvp(count_candy)
+if (game_mode == '2'):
+    pve(count_candy)
