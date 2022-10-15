96774599a063eba648d641ebb4a197b1df7bd4e8
ZhenyaGro <bon4ios@gmail.com>
2022-10-15 23:33:19 +0300

    Removed unnecessary comment

--- Seminar5/Task1.py
+++ Seminar5/Task1.py
@@ -2,10 +2,6 @@
 # File: 'Вася абв Приветабв Каабвк Тест'
 # Result: 'Вася Тест'
 
-# def read_file(path):
-#     file = open(f'{path}', 'r')
-#     return file.read().split()
-
 def read_file(path):
     file = open(f'{path}', 'r')
     return [i for i in file.read().split() if i.find('абв') == -1]
