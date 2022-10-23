3d7c74edf581e895ff7547704ebf22b673c13dd8
ZhenyaGro <bon4ios@gmail.com>
2022-10-23 21:14:02 +0300

    Using another library

--- Seminar9/Task1Miked.py
+++ Seminar9/Task1Miked.py
@@ -1,2 +1,6 @@
 # Создайте программу для игры в "Крестики-нолики" при помощи виртуального окружения и PIP
-# Using https://pypi.org/project/python-tictactoe/
+# Using https://pypi.org/project/tic-tac-toe-miked/
+
+from tic_tac_toe_miked import ttt
+
+ttt.play()
