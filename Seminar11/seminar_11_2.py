83c6ae17eb83ba93b984af2c141189e999547e55
ZhenyaGro <bon4ios@gmail.com>
2022-11-13 22:14:06 +0300

    Seminar11

--- Seminar11/seminar_11_2.py
+++ Seminar11/seminar_11_2.py
@@ -1,22 +1,47 @@
-# f(x) = -12x^4*sin(cos(x)) - 18x^3+5x^2 + 10x - 30
-
 from sympy import symbols, sin, cos
 from sympy.plotting import plot
 from scipy.optimize import fsolve
-from sympy import *
 import matplotlib.pyplot as plt
-import math
 import numpy
 
+# График функции при помощи библиотеки matplotlib:
+x = [x for x in range(-30, 30)]
+y = [(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30) for
+     x in range(-30, 30)]
+# y = [y for y in range(-30, 30)]
+plt.plot(x, y)
+plt.grid()
+plt.show()
 
+# График функции при помощи библиотеки sympy:
+# в диапазоне -300...300
+x = symbols('x')
+plot(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30,
+     (x, -300, 300))
+
+# в диапазоне -10...10
+x = symbols('x')
+plot(-12 * x ** 4 * sin(cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30,
+     (x, -10, 10))
+
+
+# Определить корни
 def f(x):
     return -12 * x ** 4 * numpy.sin(
         numpy.cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30
 
 
+# Это тригонометрическая функция, имеющая бесконечное количество корней.
+# Можно определить корни только на заданном интервале.
+# Запросим их у пользователя:
+# funcrange = list(
+#     map(int, input('Задайте интервал функции через пробел: ').split()))
 funcrange = [-10, 10]
 leftnum = min(funcrange)
 rightnum = max(funcrange)
+
+
+# Задайте интервал функции через пробел: -5 5
 
 
 def solution():
@@ -41,15 +66,7 @@ def solution():
     return roots
 
 
-def graph():
-    x = [x for x in range(-30, 30)]
-    y = [(-12 * x ** 4 * math.sin(math.cos(x)) - 18 * x ** 3 + 5 * x ** 2 + 10 * x - 30) for
-         x in range(-30, 30)]
-    plt.plot(x, y)
-    plt.grid()
-    plt.show()
-
-
+# Определить промежутки, на которых f>0 и f<0:
 def func_interval(left, right):
     array = []
     temp = left
@@ -64,6 +81,7 @@ def func_interval(left, right):
         return min(array)
 
 
+# Вычисляем координаты вершины функции на заданном интервале:
 def maxima_and_minima():
     roots = solution()
 
@@ -77,33 +95,19 @@ def maxima_and_minima():
             j = [round(i, 2) for i in j]
             print(f'Координаты вершин функции: [{j[1]}, {j[0]}]')
         if len(top) < 2:
-            print('Ошибка')
+            print('error')
         else:
             for i in range(len(top) - 1):
                 if top[i][0] > top[i + 1][0]:
-                    print(
-                        f'Функция убывает')
+                    print('Функция убывает')
                 else:
-                    print(
-                        f'Функция возрастает')
+                    print('Функция возрастает')
 
 
-print('Функция: f(x) = -12x^4*sin(cos(x)) - 18x^3+5x^2 + 10x - 30\n')
-
-# 1. Определить корни
-x_val = 0
-print('Определить корни')
-print(f'f({x_val}) = {f(x_val)}\n')
-
-
-# 2. Найти интервалы, на которых функция возрастает
-# 3. Найти интервалы, на которых функция убывает
-# 5. Вычислить вершину
-# 6. Определить промежутки, на котором f > 0
-# 7. Определить промежутки, на котором f < 0
 maxima_and_minima()
-
-# 4. Построить график
-graph()
-
-# func_interval(-10, 10)
+# Корни уравнения для заданного интервала: [-1.34, 2.27, 4.38]
+# f > 0 в промежутке (-1.34, 2.27)
+# f < 0 в промежутке (2.27, 4.38)
+# Координаты вершин функции: [-1.34, 0.11]
+# Координаты вершин функции: [2.27, -0.88]
+# Функция убывает
